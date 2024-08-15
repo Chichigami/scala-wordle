b49a4e063e2ad8afdea07f476a3a5c13441e2aad
Chichigami <g.feng.work@gmail.com>
2024-08-15 15:17:21 -0400

    error testing

--- src/test/scala/wordle-test.scala
+++ src/test/scala/wordle-test.scala
@@ -1,16 +1,19 @@
 import org.scalatest.funsuite.AnyFunSuite
 import wordle.wordle.gameStart
 import fileParse.getAnswer
+import wordle.wordle.validateGuess
 
 class WordleTest extends AnyFunSuite:
 
     test ("first word is aahed"){
         val result = getAnswer(1)
-        assert(result == "AAHED")
+        val expected = "AAHED"
+        assert(result == expected)
     }
 
     test("gameStart(69)"){
         val result = getAnswer(69)
+        val expected = "ABURN"
         assert(result == "ABURN")
     }
 
@@ -18,4 +21,38 @@ class WordleTest extends AnyFunSuite:
         val result = getAnswer(14855)
         assert(result == "ZYMIC")   
     }
+
+    test("wrap around to first word"){
+        val result = getAnswer(14856)
+        assert(result == "AAHED")
+    }
+
+    test("error number"){
+        val input = "abcd3"
+        val expected = "No numbers allowed. "
+        val actual = validateGuess(input).error
+        assert(actual == expected)
+    }
+
+    test("error length"){
+        val input = "abc"
+        val expected = "Need to be 5 letters. "
+        val actual = validateGuess(input).error
+        assert(actual == expected)
+    }
+
+    test("error special"){
+        val input = "abcd!"
+        val expected = "No special characters either."
+        val actual = validateGuess(input).error
+        assert(actual == expected)
+    }
+
+    test("error all 3"){
+        val input = "ac3@"
+        val expected = "Need to be 5 letters. No numbers allowed. No special characters either."
+        val actual = validateGuess(input).error
+        assert(actual == expected)
+    }
+
 end WordleTest
